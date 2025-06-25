2c137e0b65f52efaa46c517cd9e46f146c1e72db
alenikksss <alenaakhapkina357953@gmail.com>
2025-06-25 22:04:36 +0300

    555

---
+++ javascripts/philials.js
@@ -0,0 +1,20 @@
+// import Swiper from 'swiper';
+// import 'swiper/css'; 
+// var swiper = new Swiper('.swiper', {
+//     // Настройки для свайп-скроллинга
+//     direction: 'horizontal', // или 'vertical'
+//     loop: true, // Зацикливание слайдов
+//     pagination: {
+//       el: '.swiper-pagination',
+//     },
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+//     scrollbar: {
+//       el: '.swiper-scrollbar',
+//     },
+//     slidesPerView: 1, // Количество видимых слайдов за раз
+//     spaceBetween: 20, // Расстояние между слайдами
+//   });
+
